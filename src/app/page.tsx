0a614f4eb31fc5fab63b42d0886fb181076cea06
tukky-14 <bigoonage0104@gmail.com>
2023-12-26 20:16:11 +0900

    ログイン画面を作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,45 @@
-export default function Home() {
+import React from 'react';
+
+export default function Login() {
     return (
-        <div className="flex h-screen">
-            <div className="bg-sidebar w-64 p-4">サイドバー</div>
-            <div className="flex-1 flex flex-col">
-                <div className="bg-header h-16 p-4">ヘッダー</div>
-                <div className="bg-main flex-1 p-4">メインコンテンツ</div>
+        <div className="flex justify-center items-center h-screen bg-gray-200">
+            <div className="w-full max-w-lg p-6 rounded shadow-md bg-white">
+                <h1 className="text-lg mb-4 font-bold text-center text-indigo-600">ログイン</h1>
+                <form>
+                    <div className="mb-4">
+                        <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+                            メールアドレス
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            required
+                        />
+                    </div>
+                    <div className="mb-6">
+                        <label
+                            htmlFor="password"
+                            className="block text-sm font-medium text-gray-700"
+                        >
+                            パスワード
+                        </label>
+                        <input
+                            type="password"
+                            id="password"
+                            className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            required
+                        />
+                    </div>
+                    <div className="flex justify-center">
+                        <button
+                            type="submit"
+                            className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+                        >
+                            ログイン
+                        </button>
+                    </div>
+                </form>
             </div>
         </div>
     );

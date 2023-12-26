70ceae411518308f7da4bfd4f91835b1b4bf6518
tukky-14 <bigoonage0104@gmail.com>
2023-12-26 22:29:01 +0900

    ログイン画面を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,30 @@
+'use client';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export default function Login() {
+    const router = useRouter();
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        // ここでログイン処理を実装 (今回はスキップ)
+        // ログイン成功後、/homeにリダイレクト
+        router.push('/home');
+    };
+
     return (
         <div className="flex justify-center items-center h-screen bg-gray-200">
             <div className="w-full max-w-lg p-6 rounded shadow-md bg-white">
                 <h1 className="text-lg mb-4 font-bold text-center text-indigo-600">ログイン</h1>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">
-                            メールアドレス
+                            ユーザID
                         </label>
                         <input
-                            type="email"
-                            id="email"
-                            className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            type="text"
+                            id="userId"
+                            className="text-gray-700 mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             required
                         />
                     </div>
@@ -27,7 +38,7 @@ export default function Login() {
                         <input
                             type="password"
                             id="password"
-                            className="mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            className="text-gray-700 mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             required
                         />
                     </div>

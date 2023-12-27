c6d2c5983f40197f5cd38f5e6d43e7e7f01afa0b
tukky-14 <bigoonage0104@gmail.com>
2023-12-27 21:47:49 +0900

    TailwindCSSの並び替え処理を追加

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -10,18 +10,20 @@ export default function Home() {
 
     return (
         <div className="flex h-screen">
-            <div className="bg-sidebar w-64 p-4">サイドバー</div>
-            <div className="flex-1 flex flex-col">
-                <div className="bg-header h-16 p-4 flex justify-between items-center">
+            <div className="hidden w-64 bg-sidebar p-4 sm:block">サイドバー</div>
+            <div className="flex flex-1 flex-col">
+                <div className="flex h-16 items-center justify-between bg-header p-4">
                     <h3>ヘッダー</h3>
                     <button
                         onClick={handleLogoutClick}
-                        className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+                        className="rounded bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
                     >
                         ログアウト
                     </button>
                 </div>
-                <div className="bg-main flex-1 p-4">メインコンテンツ</div>
+                <div className="flex-1 bg-main p-4">
+                    <input className="rounded text-gray-700" maxLength={10} type="text" />
+                </div>
             </div>
         </div>
     );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,9 +12,9 @@ export default function Login() {
     };
 
     return (
-        <div className="flex justify-center items-center h-screen bg-gray-200">
-            <div className="w-full max-w-lg p-6 rounded shadow-md bg-white">
-                <h1 className="text-lg mb-4 font-bold text-center text-indigo-600">ログイン</h1>
+        <div className="flex h-screen items-center justify-center bg-gray-200">
+            <div className="w-full max-w-lg rounded bg-white p-6 shadow-md">
+                <h1 className="mb-4 text-center text-lg font-bold text-indigo-600">ログイン</h1>
                 <form onSubmit={handleSubmit}>
                     <div className="mb-4">
                         <label htmlFor="email" className="block text-sm font-medium text-gray-700">
@@ -23,7 +23,7 @@ export default function Login() {
                         <input
                             type="text"
                             id="userId"
-                            className="text-gray-700 mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            className="mt-1 block w-full rounded-md border-gray-300 p-2 text-gray-700 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             required
                         />
                     </div>
@@ -37,14 +37,14 @@ export default function Login() {
                         <input
                             type="password"
                             id="password"
-                            className="text-gray-700 mt-1 p-2 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+                            className="mt-1 block w-full rounded-md border-gray-300 p-2 text-gray-700 shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
                             required
                         />
                     </div>
                     <div className="flex justify-center">
                         <button
                             type="submit"
-                            className="px-4 py-2 bg-indigo-600 text-white rounded hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
+                            className="rounded bg-indigo-600 px-4 py-2 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-opacity-50"
                         >
                             ログイン
                         </button>

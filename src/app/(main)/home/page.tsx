41453a447c549422f25ffff86e8d01da12c4fe98
tukky-14 <bigoonage0104@gmail.com>
2024-01-07 12:15:28 +0900

    メイン画面のレイアウトを作成

--- src/app/(main)/home/page.tsx
+++ src/app/(main)/home/page.tsx
@@ -1,29 +1,21 @@
 'use client';
-import Header from '@/components/Header';
-import Sidebar from '@/components/Sidebar';
 import { useState } from 'react';
 
 export default function Home() {
     const [readonly, setReadonly] = useState(false);
 
     return (
-        <div className="flex h-screen">
-            <Sidebar />
-            <div className="flex flex-1 flex-col">
-                <Header />
-                <main className="flex-1 bg-main p-4">
-                    <input
-                        autoFocus
-                        type="text"
-                        inputMode="none"
-                        className="rounded text-gray-700"
-                        maxLength={10}
-                        readOnly={readonly}
-                        onFocus={() => setReadonly(false)}
-                        onBlur={() => setReadonly(true)}
-                    />
-                </main>
-            </div>
-        </div>
+        <>
+            <input
+                autoFocus
+                type="text"
+                inputMode="none"
+                className="rounded text-gray-700"
+                maxLength={10}
+                readOnly={readonly}
+                onFocus={() => setReadonly(false)}
+                onBlur={() => setReadonly(true)}
+            />
+        </>
     );
 }
---
+++ src/app/(main)/layout.tsx
@@ -0,0 +1,15 @@
+'use client';
+import Header from '@/components/Header';
+import Sidebar from '@/components/Sidebar';
+
+export default function MainLauout({ children }: { children: React.ReactNode }) {
+    return (
+        <div className="flex h-screen">
+            <Sidebar />
+            <div className="flex flex-1 flex-col">
+                <Header />
+                <main className="flex-1 bg-main p-4">{children}</main>
+            </div>
+        </div>
+    );
+}

30157977fce9bbc6af81e8de5a772ee89480bc7d
tukky-14 <bigoonage0104@gmail.com>
2024-01-09 22:42:39 +0900

    ページタイトルの修正

--- src/app/(main)/house/page.tsx
+++ src/app/(main)/house/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 import { useState } from 'react';
 
-export default function Home() {
+export default function House() {
     const [readonly, setReadonly] = useState(false);
 
     return (

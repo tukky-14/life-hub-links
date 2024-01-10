04b9736fdbf1ddba9f353f85e94b1c373484d0e9
tukky-14 <bigoonage0104@gmail.com>
2024-01-10 21:25:46 +0900

    初期画面を修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Login from './(certification)/signin/page';
+'use client';
+import Sidebar from '@/components/Sidebar';
+import Home from './(main)/home/page';
+import Header from '@/components/Header';
 
 export default function LoginHome() {
     return (
-        <div className="flex h-screen items-center justify-center bg-gray-200 text-gray-700">
-            <div className="w-full max-w-lg rounded bg-white p-6 shadow-md">
-                <Login />
+        <div className="flex h-screen">
+            <Sidebar />
+            <div className="flex flex-1 flex-col">
+                <Header />
+                <main className="flex-1 bg-main p-4 text-gray-700">
+                    <Home />
+                </main>
             </div>
         </div>
     );

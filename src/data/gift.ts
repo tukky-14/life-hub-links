5e8ce9c83a5996be365e14af17d11bfbfaee0949
tukky-14 <bigoonage0104@gmail.com>
2024-01-19 19:52:38 +0900

    ギフトページの画像更新

--- src/data/gift.ts
+++ src/data/gift.ts
@@ -3,7 +3,7 @@ export const GIFT_LINKS = [
         title: 'SOW EXPERIENCE',
         description:
             '興味はあっても、チャレンジする機会がないコトやモノ。そのきっかけを贈るための体験ギフト。',
-        image: 'https://giftproduction.s3.amazonaws.com/specials/list_images/5/list.jpg?1690276330',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/gift/sowxp.png',
         href: 'https://www.sowxp.co.jp/',
         tag: ['体験'],
     },
@@ -11,14 +11,14 @@ export const GIFT_LINKS = [
         title: 'アソビュー！ギフト',
         description:
             '遊び予約サイト。アウトドアスポーツやものづくり体験、遊園地や水族館、日帰り温泉などの遊び体験ギフト。',
-        image: 'https://store.asoview.com/cdn/shop/files/Fun_e720e2ed-ea11-4bdd-aea9-1c2f69783734.jpg?v=1704453819',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/gift/asoview.png',
         href: 'https://store.asoview.com/',
         tag: ['体験'],
     },
     {
         title: 'dozo',
         description: '相手にぴったりのギフトをテーマというカタチで贈るえらべるギフトのサービス。',
-        image: 'https://dozo-gift.com/cdn/shop/t/2/assets/img_home_1000.png?v=156512717646350201681650620718',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/gift/dozo.png',
         href: 'https://dozo-gift.com/',
         tag: ['食べ物', '体験'],
     },
@@ -26,7 +26,7 @@ export const GIFT_LINKS = [
         title: 'snaq me',
         description:
             'ワクワクおやつの定期便。身体に不必要なものできるだけ使わない、やさしいおやつを届けるサービス。',
-        image: 'https://images.microcms-assets.io/assets/3772011fae70431ea2fc1f48b3ab2bf2/ee5910f560cd49f2a863ecf23835bf35/220228_Up_01%202.jpg?fit=crop&w=500&h=500',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/gift/snaqme.png',
         href: 'https://snaq.me/',
         tag: ['食べ物'],
     },

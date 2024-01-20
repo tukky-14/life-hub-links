a9865922f86324781ed2a8249e8df6e322d6676d
tukky-14 <bigoonage0104@gmail.com>
2024-01-20 10:39:10 +0900

    住居ページのリンクを変更

--- src/data/house.ts
+++ src/data/house.ts
@@ -1,29 +1,30 @@
+// https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/house/xxxxx.png
 export const HOUSE_LINKS = [
     {
         title: 'ガッコム安全ナビ',
         description: '不審者情報検索サイト',
-        image: 'https://www.gaccom.jp/img/safety_map/anzennavi-normal-map.png?aa',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/house/gaccom.png',
         href: 'https://www.gaccom.jp/safety/',
         tag: ['安全'],
     },
     {
         title: '大島てる',
         description: '事故物件検索サイト',
-        image: 'https://www.oshimaland.co.jp/images/logos/logo.svg',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/house/oshimaland.png',
         href: 'https://www.oshimaland.co.jp/',
         tag: ['安全'],
     },
     {
         title: 'マンションノート',
         description: '日本最大級のマンション口コミサイト',
-        image: 'https://www.mansion-note.com/static/img/svg/logo_large.svg',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/house/mansion-note.png',
         href: 'https://www.mansion-note.com/',
         tag: ['安全'],
     },
     {
         title: 'ポストマップ',
         description: '郵便ポストの検索',
-        image: 'https://kotobank.jp/image/dictionary/daijisen/media/108380L.jpg',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/house/postmap.png',
         href: 'https://www.postmap.org/',
         tag: ['安全'],
     },

1d141055660c273d21178b8b79b7163a4d42eafb
tukky-14 <bigoonage0104@gmail.com>
2024-01-20 21:50:32 +0900

    料理ページを更新

--- src/data/cooking.ts
+++ src/data/cooking.ts
@@ -1,9 +1,48 @@
+// https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/xxxxx.png
+
 export const COOKING_LINKS = [
     {
-        title: '',
-        description: '',
-        image: '',
+        title: 'バズレシピ',
+        description: '料理研究家・リュウジがこれまでネット上で発表してきた1200以上のレシピを検索。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/bazurecipe.png',
+        href: 'https://bazurecipe.com/',
+        tag: ['簡単'],
+    },
+    {
+        title: 'クックパッド',
+        description:
+            '言わずと知れた料理レシピのコミュニティウェブサイト。毎日の料理を楽しみにする。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/cookpad.png',
+        href: 'https://cookpad.com/',
+        tag: [''],
+    },
+    {
+        title: 'DELISH KITCHEN',
+        description: '料理レシピ動画で作り方が簡単にわかるレシピサイト',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/delishkitchen.png',
         href: '',
+        tag: [''],
+    },
+    {
+        title: 'レシピサイトNadia',
+        description:
+            'プロの料理家、料理研究家、フードコーディネーターたちのとっておきレシピや料理コラムを毎日お届け。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/nadia.png',
+        href: 'https://oceans-nadia.com/',
+        tag: [''],
+    },
+    {
+        title: 'クラシル',
+        description: '',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/kurashiru.png',
+        href: 'https://www.kurashiru.com/',
+        tag: [''],
+    },
+    {
+        title: '白ごはん.com',
+        description: '',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/sirogohan.png',
+        href: 'https://www.sirogohan.com/',
         tag: [''],
     },
 ];

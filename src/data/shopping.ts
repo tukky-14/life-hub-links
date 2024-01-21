dc8388dc456fe0542b93e3c536134423cdb511ce
tukky-14 <bigoonage0104@gmail.com>
2024-01-21 16:09:13 +0900

    買い物ページを更新

--- src/data/shopping.ts
+++ src/data/shopping.ts
@@ -3,10 +3,27 @@
 
 export const SHOPPING_LINKS = [
     {
-        title: '',
-        description: '',
-        image: '',
-        href: '',
+        title: '価格.com',
+        description:
+            'さまざまなジャンルの商品を比較することのできるサービスを提供しているWebサイト。扱われる商品はパソコンや周辺機器をはじめ、家具や家電など多岐にわたる。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/shopping/kakaku.png',
+        href: 'https://kakaku.com/',
+        tag: [''],
+    },
+    {
+        title: 'トリセツ',
+        description:
+            'さまざまな製品の取扱説明書を PC・スマホで一元管理、「いつでも・どこでも」閲覧できるサービス。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/shopping/torisetsu.png',
+        href: 'https://torisetsu.biz/',
+        tag: [''],
+    },
+    {
+        title: '家電批評モノマニア',
+        description:
+            '文系研究者による最新家電と電化製品の比較と批評のブログ。家電購入の参考になる情報が盛りだくさん。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/shopping/monomania.png',
+        href: 'http://monomania.sblo.jp/',
         tag: [''],
     },
 ];

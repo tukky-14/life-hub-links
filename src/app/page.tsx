c67d635fea443d177d092e035d4b505051d7341c
tukky-14 <bigoonage0104@gmail.com>
2024-01-21 16:50:19 +0900

    トップページを更新

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ const AfterLoginHome = () => {
             <div className="flex flex-1 flex-col bg-main text-gray-700">
                 <Header />
                 <main className="flex-1 p-4">
-                    <div></div>
+                    <div>生活に便利なリンクを集めました。</div>
                 </main>
             </div>
         </div>

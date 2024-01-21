1f0551af9d9dce0df87c33ccad456f1281102620
tukky-14 <bigoonage0104@gmail.com>
2024-01-21 19:24:23 +0900

    スマホ・タブレットでホーム画面に追加して起動したとき、アドレスバーを表示させない

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ export const metadata: Metadata = {
     title: 'LifeHub Links',
     description: 'さまざまな生活関連のリンクを中心に集めた情報ハブ。',
     keywords: 'next.js, react, web development',
+    appleWebApp: true,
 };
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {

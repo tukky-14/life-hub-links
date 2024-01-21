a274797160150f264614914ddc26319ff7f14219
tukky-14 <bigoonage0104@gmail.com>
2024-01-21 16:21:54 +0900

    お金ページを更新

--- src/data/money.ts
+++ src/data/money.ts
@@ -3,10 +3,17 @@
 
 export const MONEY_LINKS = [
     {
-        title: '',
-        description: '',
-        image: '',
-        href: '',
+        title: 'リベラルアーツ大学',
+        description: '日本一自由なIT会社の社長が『自由に生きるための知恵』を発信しているブログ。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/money/liberal.png',
+        href: 'https://liberaluni.com/',
+        tag: [''],
+    },
+    {
+        title: '金融庁(社会人になる方へ)',
+        description: '金融庁のお金の勉強になる資料のまとめ。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/money/kinyu.png',
+        href: 'https://www.fsa.go.jp/teach/syakaijin.html',
         tag: [''],
     },
 ];

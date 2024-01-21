9c61d2d2fc6258d0c3910d7cf6ca38c93a3fe36e
tukky-14 <bigoonage0104@gmail.com>
2024-01-21 15:54:40 +0900

    育児ページを更新

--- src/data/childcare.ts
+++ src/data/childcare.ts
@@ -3,10 +3,99 @@
 
 export const CHILDCARE_LINKS = [
     {
-        title: '',
-        description: '',
-        image: '',
-        href: '',
+        title: 'たまひよ',
+        description:
+            '「たまひよ」の雑誌・書籍・ネットから妊娠・出産・育児に関する最新ニュースやコラム、キャンペーン、イベント等を発信。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/tamahiyo.png',
+        href: 'https://st.benesse.ne.jp/',
+        tag: [''],
+    },
+    {
+        title: 'ベビーカレンダー',
+        description:
+            '「赤ちゃんの笑顔でいっぱいの毎日を」をテーマに妊娠・出産・育児の情報を日がわりで配信。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/babycalendar.png',
+        href: 'https://baby-calendar.jp/',
+        tag: [''],
+    },
+    {
+        title: 'ママスタセレクト',
+        description:
+            '「いつでも、どこでも、ママに寄り添う情報を」をコンセプトに、子育て情報からママ友、旦那、義母の話、教育コラム、行政の育児支援などを日々配信。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/mamastarselect.png',
+        href: 'https://select.mamastar.jp/',
+        tag: [''],
+    },
+    {
+        title: 'コノビー',
+        description:
+            '子どもの個性が伸びる方向を見つけ、それぞれに合った子育てのスタイルを創り出すきっかけとなるような子育てに関する情報を発信するウェブメディア。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/conobie.png',
+        href: 'https://conobie.jp/',
+        tag: [''],
+    },
+    {
+        title: 'hapimama(ハピママ)',
+        description:
+            'ママたちと作る、ママのためのイベント。 「ハピママコラム」は、ママのための情報のシェア、課題をともに考える“きっかけ”づくりの場。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/hapimama.png',
+        href: 'https://ure.pia.co.jp/list/media?c=hapimama',
+        tag: [''],
+    },
+    {
+        title: 'cozre(コズレ)',
+        description:
+            '赤ちゃんや子供とのおでかけ先や習い事、子供服から夜泣きまで出産以降を中心とした子育ての悩みを解決する、子育てナレッジシェアメディア。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/cozre.png',
+        href: 'https://feature.cozre.jp/',
+        tag: [''],
+    },
+    {
+        title: 'COKARA',
+        description:
+            '【こども・おやこ】をテーマに「"#子育て "を通して親が満たされていく社会」を目指す会社のメディア。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/cokara.png',
+        href: 'https://media.postmate.jp/',
+        tag: [''],
+    },
+    {
+        title: 'KIDSNA STYLE',
+        description:
+            '「育てるを考える」250万人が利用する子育て情報アプリ。 妊活・妊娠・出産・子育てなど、ママ・パパのお悩みを解決する情報を配信。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/kidsna.png',
+        href: 'https://kidsna.com/magazine',
+        tag: [''],
+    },
+    {
+        title: 'トモニテ',
+        description:
+            '「子育てを通じて、人が、社会が、ともに手をとりあう世界」を目指すファミリー向け動画メディア。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/tomonite.png',
+        href: 'https://tomonite.com/',
+        tag: [''],
+    },
+    {
+        title: 'アイデスマガジン',
+        description:
+            'お子様の成長の瞬間をより輝かせるために。自転車や三輪車、お遊びから子育てまで幅広いコンテンツを掲載。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/idesnet.png',
+        href: 'https://www.idesnet.co.jp/magazine/',
+        tag: [''],
+    },
+    {
+        title: 'マイナビ子育て',
+        description:
+            '「夫婦一緒に子育て」マイナビ子育ては、働くパパママたちに寄り添い、さまざまなお手伝いを目指すメディア。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/mynavi.png',
+        href: 'https://woman.mynavi.jp/kosodate/',
+        tag: [''],
+    },
+    {
+        title: '絵本ナビスタイル',
+        description:
+            '絵本・児童書や子育てに関するニュースやお役立ち情報をお届けするサイト。最新情報から、読み聞かせ、読書感想文、知育や英語教育、お出かけ情報まで毎日更新。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/childcare/ehonnavi.png',
+        href: 'https://style.ehonnavi.net/',
         tag: [''],
     },
 ];

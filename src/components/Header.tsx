d6ab850c2aee81994884222c4e8d302b3aef7f7d
tukky-14 <bigoonage0104@gmail.com>
2024-01-21 17:32:27 +0900

    ハンバーガーメニューを追加

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,47 @@ import { signOut } from '@/hooks/useAuth';
 import { useRouter } from 'next/navigation';
 import { useSidebar } from '@/hooks/useSidebar';
 
+import { RxHamburgerMenu } from 'react-icons/rx';
+import { useState } from 'react';
+import Link from 'next/link';
+import { SIDEBAR_OPTIONS } from '@/data/sidebar';
+
 const Header = () => {
     const router = useRouter();
-    const { sidebarOption } = useSidebar();
+    const [menuOpen, setMenuOpen] = useState(false);
 
     const handleLogoutClick = async () => {
         await signOut();
         router.push('/');
     };
 
+    const handleMenuClick = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <header className="flex h-16 items-center justify-between border-b border-gray-300 p-4">
-            <div className="text-sm italic">{sidebarOption?.proverb}</div>
+        <header className="relative flex h-16 items-center justify-between border-b border-gray-300 py-4">
+            <Link href="/" className="ml-1 pl-4 text-xl font-bold sm:hidden">
+                LifeHub Links
+            </Link>
+            <button className="pr-4 sm:hidden" onClick={handleMenuClick}>
+                <RxHamburgerMenu size="2rem" />
+            </button>
             {/* <Button type="button" id="logout" text="ログアウト" onClick={handleLogoutClick} /> */}
+            {menuOpen && (
+                <div className="absolute top-16 z-10 flex w-full flex-col bg-gray-300 md:hidden">
+                    {SIDEBAR_OPTIONS.map((option) => (
+                        <Link
+                            className="flex items-center border-b py-2 pl-3 hover:bg-gray-200"
+                            href={option.href}
+                            key={option.label}
+                        >
+                            {option.icon}
+                            <p>{option.label}</p>
+                        </Link>
+                    ))}
+                </div>
+            )}
         </header>
     );
 };

5b142433793a455492af250c17afe6055062d1c6
tukky-14 <bigoonage0104@gmail.com>
2024-01-22 19:47:20 +0900

    料理ページを更新

--- src/data/cooking.ts
+++ src/data/cooking.ts
@@ -3,13 +3,6 @@
 
 export const COOKING_LINKS = [
     {
-        title: 'バズレシピ',
-        description: '料理研究家・リュウジがこれまでネット上で発表してきた1200以上のレシピを検索。',
-        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/bazurecipe.png',
-        href: 'https://bazurecipe.com/',
-        tag: ['簡単'],
-    },
-    {
         title: 'クックパッド',
         description:
             '言わずと知れた料理レシピのコミュニティウェブサイト。毎日の料理を楽しみにする。',
@@ -25,6 +18,13 @@ export const COOKING_LINKS = [
         tag: [''],
     },
     {
+        title: 'クラシル',
+        description: '',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/kurashiru.png',
+        href: 'https://www.kurashiru.com/',
+        tag: [''],
+    },
+    {
         title: 'レシピサイトNadia',
         description:
             'プロの料理家、料理研究家、フードコーディネーターたちのとっておきレシピや料理コラムを毎日お届け。',
@@ -33,17 +33,17 @@ export const COOKING_LINKS = [
         tag: [''],
     },
     {
-        title: 'クラシル',
-        description: '',
-        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/kurashiru.png',
-        href: 'https://www.kurashiru.com/',
-        tag: [''],
-    },
-    {
         title: '白ごはん.com',
         description: '',
         image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/sirogohan.png',
         href: 'https://www.sirogohan.com/',
         tag: [''],
+    },
+    {
+        title: 'バズレシピ',
+        description: '料理研究家・リュウジがこれまでネット上で発表してきた1200以上のレシピを検索。',
+        image: 'https://life-hub-links-images.s3.ap-northeast-1.amazonaws.com/cooking/bazurecipe.png',
+        href: 'https://bazurecipe.com/',
+        tag: ['簡単'],
     },
 ];

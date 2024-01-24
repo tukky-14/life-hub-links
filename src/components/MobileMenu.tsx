59c7ab86fc88b0fa8f7c8b1f0210b78f48ee9c04
tukky-14 <bigoonage0104@gmail.com>
2024-01-24 19:03:19 +0900

    モバイルメニューの項目をクリックしたとき、メニューが閉じない問題を修正

--- src/components/MobileMenu.tsx
+++ src/components/MobileMenu.tsx
@@ -1,13 +1,27 @@
 import { SIDEBAR_OPTIONS } from '@/data/sidebar';
-import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { RxHamburgerMenu } from 'react-icons/rx';
 
 const MobileMenu = () => {
+    const router = useRouter();
     const [menuOpen, setMenuOpen] = useState(false);
 
+    /**
+     * メニューの開閉
+     */
     const handleMenuClick = () => {
         setMenuOpen(!menuOpen);
+    };
+
+    /**
+     * メニューの項目をクリックしたときの処理
+     * @param event
+     */
+    const handleMenuOptionClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+        const { name } = event.currentTarget;
+        setMenuOpen(false);
+        router.push(name);
     };
 
     return (
@@ -19,14 +33,15 @@ const MobileMenu = () => {
             {menuOpen && (
                 <div className="absolute top-16 z-10 flex w-full flex-col bg-gray-300 md:hidden">
                     {SIDEBAR_OPTIONS.map((option) => (
-                        <Link
+                        <button
                             className="flex items-center border-b py-2 pl-3 hover:bg-gray-200"
-                            href={option.href}
+                            name={option.href}
                             key={option.label}
+                            onClick={handleMenuOptionClick}
                         >
                             {option.icon}
                             <p>{option.label}</p>
-                        </Link>
+                        </button>
                     ))}
                 </div>
             )}
